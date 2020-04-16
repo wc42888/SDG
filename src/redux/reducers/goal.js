dc08deba42db22b0b1650a76d55224837bf0714f
chao wang <wc42888@hotmail.com>
2020-04-16 22:48:17 +1000

    updated reducer

--- src/redux/reducers/goal.js
+++ src/redux/reducers/goal.js
@@ -1,7 +1,11 @@
+import {GET_GOALS_SUCCESS} from '../actions';
+
 const initialState = [];
 
 const goalReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_GOALS_SUCCESS:
+      return action.payload.goals;
     default:
       return state;
   }
