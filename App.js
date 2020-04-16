4848c299f85bd62c7f7dc9afa236fc5bd186420e
chao wang <wc42888@hotmail.com>
2020-04-16 22:45:02 +1000

    wrapped the app with redux provider

--- App.js
+++ App.js
@@ -9,18 +9,22 @@
 import React from 'react';
 import {StatusBar, SafeAreaView, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
 import MainApp from './src/screens';
 
 const App = () => {
   const {safeAreaView} = styles;
 
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={safeAreaView}>
-        <MainApp />
-      </SafeAreaView>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={safeAreaView}>
+          <MainApp />
+        </SafeAreaView>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
