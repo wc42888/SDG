7f32626e9e231c0295235a2ec954406ccafab66c
chao wang <wc42888@hotmail.com>
2020-04-17 10:02:52 +1000

    added a new config for finding goal name

---
+++ src/config/goalName.js
@@ -0,0 +1,21 @@
+const goalNames = [
+  {code: '1', name: 'No POVERTY'},
+  {code: '2', name: 'ZERO HUNGER'},
+  {code: '3', name: 'GOOD HEALTH AND WELL-BEING'},
+  {code: '4', name: 'QUALITY EDUCATION'},
+  {code: '5', name: 'GENDER EQUALITY'},
+  {code: '6', name: 'CLEAN WATER AND SANTITATION'},
+  {code: '7', name: 'AFFORDABLE AND CLEAN ENERGY'},
+  {code: '8', name: 'DECENT WORK AND ECONOMIC GROWTH'},
+  {code: '9', name: 'INDUSTRY, INNOVATION AND INFRASTRUCTURE'},
+  {code: '10', name: 'REDUCED INEQUALITIES'},
+  {code: '11', name: 'SUSTANABLE CITIES AND COMMUNITIES'},
+  {code: '12', name: 'RESPONSIBLE CONSUMPTION AND PROTECTION'},
+  {code: '13', name: 'CLIMATE ACTION'},
+  {code: '14', name: 'LIFE BELOW WATER'},
+  {code: '15', name: 'LIFE AND LAND'},
+  {code: '16', name: 'PEACH, JUSTICE AND STRONG INSTITUTIONS'},
+  {code: '17', name: 'PARTNERSHIPS FOR THE GOALS'},
+];
+
+export default goalNames;
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 import {BASE_URL} from './api';
 import {LARGE_SIZE, NORMAL_SIZE} from './font';
+import goalName from './goalName';
 
-export {BASE_URL, LARGE_SIZE, NORMAL_SIZE};
+export {BASE_URL, LARGE_SIZE, NORMAL_SIZE, goalName};
