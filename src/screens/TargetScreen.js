1f24f8e4c11cc073759a348dc7d86c3a09f62d89
chao wang <wc42888@hotmail.com>
2020-04-17 12:40:59 +1000

    updated target screen

--- src/screens/TargetScreen.js
+++ src/screens/TargetScreen.js
@@ -1,6 +1,62 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {getAllTargets} from '../redux/actions';
+import {TargetCard} from '../components';
 
-const TargetScreen = () => <Text>target screen</Text>;
+// eslint-disable-next-line react/prop-types
+const TargetScreen = ({route}) => {
+  const {root, emptyList} = styles;
+  const {
+    params: {goal},
+  } = route;
+  const dispatch = useDispatch();
+  const targets = useSelector((state) => state.target);
+  const isLoadingTargets = useSelector((state) => state.loading.GET_TARGETS);
+
+  const fetchTargets = () => dispatch(getAllTargets(goal.code));
+
+  useEffect(() => {
+    fetchTargets();
+  }, []);
+
+  const keyExtractor = (item) => item.code;
+
+  // eslint-disable-next-line react/prop-types
+  const renderItem = ({item}) => {
+    return <TargetCard target={item} />;
+  };
+
+  const ListEmptyComponent = () => (
+    <View style={emptyList}>
+      <Text>No target information available, please try to refresh</Text>
+    </View>
+  );
+
+  return (
+    <View style={root}>
+      <FlatList
+        ListEmptyComponent={ListEmptyComponent}
+        data={targets}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        refreshing={isLoadingTargets}
+        onRefresh={fetchTargets}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    width: '100%',
+  },
+  emptyList: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default TargetScreen;
