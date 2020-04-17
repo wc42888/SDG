6b61e36d4bab3246400075a8474ba6158af6ee96
chao wang <wc42888@hotmail.com>
2020-04-17 11:00:10 +1000

    added loading reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 import goal from './goal';
+import loading from './loading';
 
-const rootReducer = combineReducers({goal});
+const rootReducer = combineReducers({goal, loading});
 
 export default rootReducer;
