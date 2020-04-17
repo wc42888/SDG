5e375e381d6fac6539729f0506da90271766cccd
chao wang <wc42888@hotmail.com>
2020-04-17 10:03:05 +1000

    updated home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,15 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {Text, ImageBackground, StyleSheet, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
 import {getAllGoals} from '../redux/actions';
-import {LARGE_SIZE} from '../config';
+import {LARGE_SIZE, goalName} from '../config';
+import {GoalTile} from '../components';
 
 const HomeScreen = () => {
   const {backgroundImage, content, title} = styles;
@@ -17,29 +24,55 @@ const HomeScreen = () => {
 
   const renderTitle = () => <Text style={title}>{getTitleText()}</Text>;
 
+  const findGoalName = (code) =>
+    goalName.find((goalItem) => goalItem.code === code).name;
+
+  const addGoalName = (goal) => ({...goal, name: findGoalName(goal.code)});
+
+  const renderGoalTiles = () => (
+    <View style={styles.goalsSection}>
+      {goals.length
+        ? goals.map((goal) => (
+            <GoalTile
+              key={goal.code}
+              goal={addGoalName(goal)}
+              disabled={goal.code !== '13'}
+            />
+          ))
+        : null}
+    </View>
+  );
+
   return (
     <ImageBackground
       // eslint-disable-next-line global-require
       source={require('../assets/images/sustain.jpg')}
       style={backgroundImage}>
-      <ScrollView style={content}>{renderTitle()}</ScrollView>
+      {renderTitle()}
+      <ScrollView style={content}>{renderGoalTiles()}</ScrollView>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   backgroundImage: {
-    flex: 1,
+    flexGrow: 1,
     width: '100%',
     height: 400,
   },
   content: {
-    paddingTop: 200,
     paddingHorizontal: 10,
   },
   title: {
+    marginTop: 200,
     fontSize: LARGE_SIZE,
     color: 'white',
+  },
+  goalsSection: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 50,
+    justifyContent: 'space-around',
   },
 });
 
